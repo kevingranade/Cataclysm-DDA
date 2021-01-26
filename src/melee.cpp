2e3d3971ab84fd26df5de53f3829285b8bf57525
Kevin Granade <kevin.granade@gmail.com>
2021-01-26 13:37:17 -0800

    Fix unecessary scaling to melee skill cap

--- src/melee.cpp
+++ src/melee.cpp
@@ -1004,7 +1004,7 @@ float Character::get_dodge() const
     //Dodge decreases linearly to 0 when below 50% stamina.
     const float stamina_ratio = static_cast<float>( get_stamina() ) / get_stamina_max();
     if( stamina_ratio <= .5 ) {
-        ret *= 2 * stamina_ratio;
+        ret *= 2.0f * stamina_ratio;
     }
 
     return std::max( 0.0f, ret );
